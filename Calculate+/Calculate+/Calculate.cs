09c9a2b93141fb57af08a915f986e8c227fc35a7
pht5050 <72013830+pht5050@users.noreply.github.com>
2020-10-12 21:12:52 +0800

    Add files via upload

---
+++ Calculate+/Calculate+/Calculate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculate_
+{
+    class Calculate
+    {
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("0不能作为除数!");
+                return 0;
+            }
+            else
+                return a / b;
+        }
+        public bool Equals(double a, double b)
+        {
+            if (a == b)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Calculate+/Calculate+/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculate_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+            Console.WriteLine("请分别输入两个数字");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}", num1, num2, c.Addition(num1, num2));
+            Console.WriteLine("{0}-{1}={2}", num1, num2, c.Subtraction(num1, num2));
+            Console.WriteLine("{0}x{1}={2}", num1, num2, c.Multiplication(num1, num2));
+            Console.WriteLine("{0}/{1}={2}", num1, num2, c.Division(num1, num2));
+            Console.WriteLine(c.Equals(num1, num2));
+            Character ch = new Character();
+            Console.WriteLine("请分别输入两个字符串");
+            string a = Convert.ToString(Console.ReadLine());
+            string b = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}", a,b, ch.Addition(a, b));
+            Console.WriteLine("{0}-{1}={2}", a, b, ch.Subtraction(a,b));
+        }
+    }
+}
